f6933ae54fafff8a8a6c9f7d1e27f3bc940c1442
vernonn <695035427@qq.com>
2019-02-22 11:54:35 +0800

    todolist base

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,69 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import TodoItem from './TodoItem';
 
 class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: '',
+      list: ['React', 'Vue']
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleButtonClick = this.handleButtonClick.bind(this)
+    this.handleItemDel = this.handleItemDel.bind(this)
+  }
+
   render() {
+    const { inputValue } = this.state;
     return (
-      <div>
-        tdls
-      </div>
+      <Fragment>
+        <div>
+          {/* 输入框 */}
+          <label htmlFor="input">输入TODO</label>
+          <input
+            id="input"
+            value={inputValue}
+            onChange={this.handleInputChange}
+          />
+          <button onClick={this.handleButtonClick}>提交</button>
+        </div>
+        <ul>
+          {this.getTodoItem()}
+        </ul>
+      </Fragment>
     );
+  }
+
+  getTodoItem() {
+    return this.state.list.map((item, idx) => {
+      return (
+        <Fragment key={idx}>
+          <TodoItem content={item} index={idx} delItem={this.handleItemDel}/>
+        </Fragment>
+      )
+    })
+  }
+  // 使用ES6的箭头函数就不需要在JSX中使用.bind(this)
+  handleInputChange(e) {
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+  handleButtonClick() {
+    const { inputValue, list } = this.state;
+    this.setState({
+      list: [...list, inputValue],
+      inputValue: ''
+    })
+  }
+  handleItemDel(idx) {
+    const { list } = this.state;
+    const newList = [...list]
+    newList.splice(idx, 1)
+    this.setState({
+      list: [...newList],
+    })
   }
 }
 
